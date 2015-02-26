272479407cc16857e86d2fef0b5cb6d859229e87
Meghan Smith <meghansmithsc@gmail.com>
2015-02-26 17:09:40 -0500

    playing with .js menus

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+	
+// 	$(".menuHide").addClass('hide');
+// 	$('#navButton').click(function(){
+// 		$(".menuHide").slideToggle('fast');
+// 	});
+
+// 	$(function () {
+// 	    $(window).scroll(function () {
+// 	        if ($(this).scrollTop() > 480) {
+// 	            $('.fixIt').addClass('fixed');
+// 	        } else if ($(this).scrollTop() <= 480) {
+// 	            $('.fixIt').removeClass('fixed');
+// 	        }
+// 	    });
+// 	});
+// });
