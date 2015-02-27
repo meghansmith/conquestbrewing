12b7c0a8dd0787b9bd9b7d4db736918417f3d983
Meghan Smith <meghansmithsc@gmail.com>
2015-02-27 12:35:19 -0500

    Update styles, add .js for sticky nav

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 	
-// 	$(".menuHide").addClass('hide');
-// 	$('#navButton').click(function(){
-// 		$(".menuHide").slideToggle('fast');
-// 	});
+	$(".menuHide").addClass('hide');
+	$('#navButton').click(function(){
+		$(".menuHide").slideToggle('fast');
+	});
 
-// 	$(function () {
-// 	    $(window).scroll(function () {
-// 	        if ($(this).scrollTop() > 480) {
-// 	            $('.fixIt').addClass('fixed');
-// 	        } else if ($(this).scrollTop() <= 480) {
-// 	            $('.fixIt').removeClass('fixed');
-// 	        }
-// 	    });
-// 	});
-// });
+	$(function () {
+	    $(window).scroll(function () {
+	        if ($(this).scrollTop() > 575) {
+	        	$('body').removeClass('notFixed');
+	            $('body').addClass('fixed');
+	        } else if ($(this).scrollTop() <= 575) {
+	            $('body').removeClass('fixed');
+	            $('body').addClass('notFixed');
+
+	        }
+	    });
+	});
+	$('a').click(function(){
+   $('html, body').animate({
+       scrollTop: $( $.attr(this, 'href') ).offset().top
+   }, 500);
+   return false;
+});
+});
